41e780294fc6e6ba84ba99d57920c9e149d24f25
Meera Patel <meera.patel2984@gmail.com>
2021-08-12 15:32:33 +0100

    Shows act warnings when simulating input change

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { useFormik } from "formik";
 
 function App() {
+  const { handleChange, values, handleSubmit } = useFormik({
+    initialValues: { pokemonName: "" },
+    onSubmit: () => {},
+  });
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <form onSubmit={handleSubmit}>
+          <label>
+            Pokemon
+            <input
+              name="pokemonName"
+              onChange={handleChange}
+              value={values.pokemonName}
+            />
+          </label>
+          <button type="submit">Gotta catch 'em all</button>
+        </form>
       </header>
     </div>
   );
